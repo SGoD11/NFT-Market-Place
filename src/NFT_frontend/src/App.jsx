7085090b935c0a1868d2b7db968f056bccaecb71
SGoD11 <subhajit13dhar@gmail.com>
2025-04-03 12:36:52 +0000

    set up the project

--- src/NFT_frontend/src/App.jsx
+++ src/NFT_frontend/src/App.jsx
@@ -1,30 +1,19 @@
 import { useState } from 'react';
-import { NFT_backend } from 'declarations/NFT_backend';
-
+import { NFT_backend as backend} from 'declarations/NFT_backend';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import homeImage from "../dist/home-img.png";
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
-  const [greeting, setGreeting] = useState('');
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    NFT_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
-    return false;
-  }
+ 
 
   return (
-    <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
-      </form>
-      <section id="greeting">{greeting}</section>
-    </main>
+    <div className="App">
+      <Header />
+      <img className="bottom-space" src={homeImage} />
+      <Footer />
+    </div>
   );
 }
 
