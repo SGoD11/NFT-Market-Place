908a3622be935fc86ce42d97c7d9945e131a2fea
SGoD11 <subhajit13dhar@gmail.com>
2025-04-05 11:20:48 +0000

    dfx identity get-principal setup done

--- src/NFT_frontend/src/App.jsx
+++ src/NFT_frontend/src/App.jsx
@@ -1,28 +1,19 @@
 import { useEffect, useState } from 'react';
-import { NFT } from 'declarations/NFT';
+import { NFT_canister as NFT } from 'declarations/NFT_canister';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import homeImage from "/home-img.png";
 import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
 
-  const [Notes, setNotes] = useState('');
-  useEffect(()=>{
-    const fetch = async()=>{
-      const fetchedNotes = await NFT.greet();
-      setNotes(fetchedNotes);
-      await NFT.insert(fetchedNotes);
-      
-    };
-    fetch();
-  },[]);
+
 
 
   return (
     <div className="App">
       <Header />
       <img className="bottom-space" src={homeImage} />
-      {Notes}
+      
       <Footer />
     </div>
   );
